81ce4463530bcb07c74224dd06a638bb9669e66d
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-02-24 17:22:54 +0200

    Fixed: Navbar border

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ export const revalidate = 0;
 const Navbar = async () => {
     const categories = await getCategories();
     return (
-        <nav className="border-t">
+        <nav className="border-b">
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
