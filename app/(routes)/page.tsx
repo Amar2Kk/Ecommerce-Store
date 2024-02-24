4221b3d7ca2ff8f3107e9b9c87a5e8e470ec15a3
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-02-24 17:18:00 +0200

    Featured Products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+export default async function Home() {
+    const billboard = await getBillboards(
+        "0e7d619f-c0f0-4458-b625-9bd23b062d2d"
+    );
+    const products = await getProducts({ isFeatured: true });
+
     return (
-        <main>
-            <div>Home</div>
-        </main>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+        </Container>
     );
 }
